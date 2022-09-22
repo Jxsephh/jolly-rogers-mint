1abb3b3d112cea1fb235e4982a3f0a9f1e7d01dd
Joseph <josephsantanapr@gmail.com>
2022-09-21 21:07:04 -0500

    Fixed spacing for description

--- components/Collection.tsx
+++ components/Collection.tsx
@@ -56,7 +56,7 @@ export function Collection({
             {collection.name}
           </Text>
           <Paragraph className={wrapWords} mb="x2">
-            <ReactMarkdown>{JSON.parse(`"${metadata?.description || collection?.editionMetadata?.description}"`)}</ReactMarkdown>
+		  The official logo, symbol, meme, mascot, and image of TORTUGA. <br/><br/> ↳ Year: 2022 <br/> ↳ Medium: Pixels on Digital Canvas ↳ Dimensions: 3000 × 3000 px <br/> ↳ License: Creative Commons Zero (CC0) <br/>↳ Artist: Saucedo.eth <br/>↳ Reference(s): Black Square by Kazimir Malevich DONDA by Ye One Piece by Eiichiro Oda X-ray of CryptoPunk 130 by PIV
           </Paragraph>
         </Stack>
 
